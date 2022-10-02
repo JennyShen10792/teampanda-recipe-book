508d2e57a51a7e9854d97e7e2a6809af9ba4ba82
jiongyunsun <js11116@nyu.edu>
2022-10-02 17:05:33 -0400

    Creating saving feature

---
+++ Save.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Save{
+
+	public void save(Recipe r) {
+		try {
+			FileWriter fw=new FileWriter(r.getTitle() + ".txt");
+            fw.write("Title: "+r.getTitle()+"\n" +"Ingredients: "+r.getIngredient()+'\n'+"Instructions: "+r.getInstrcution());
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String get_recipe(String t) {
+		Scanner scanner;
+		String result = "";
+		try {
+			scanner = new Scanner(new File(t+".txt"));
+		while(scanner.hasNextLine())
+			{
+				result+= scanner.nextLine()+"\n";
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+}
