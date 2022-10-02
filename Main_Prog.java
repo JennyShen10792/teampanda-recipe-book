95ab86df2688a6e5f3913c1710b11235aa469911
Harry Liu <harryliu23333@gmail.com>
2022-10-02 14:31:19 -0400

    Updated Creation and Saving Feature

--- Main_Prog.java
+++ Main_Prog.java
@@ -8,30 +8,34 @@ public class Main_Prog {
         System.out.println("Hey there! This is your digital Recipe Book. Press \"c\" to create a recipe, ...:");
         String option;
 
-        while ((option = in.nextLine()).equals("q") == false) {
+        while (!(option = in.nextLine()).equals("q")) {
             if (option.equals("c")) {
-                System.out.print("Enter a recipe name: \n");
+                System.out.println("Enter a recipe name: ");
                 String recipe_name = in.nextLine();
+                System.out.println();
 
-                System.out.print("Enter a description of the recipe: \n");
+                System.out.println("Enter a description of the recipe: ");
                 String descript = in.nextLine();
+                System.out.println();
 
                 System.out.println("Enter a list of ingredients needed (enter \"end\" to quit): ");
                 ArrayList<String> ingred_list = new ArrayList<>();
                 String new_ingred;
-                while ((new_ingred = in.next()).equals("end") == false) {
+                while ((new_ingred = in.nextLine()).equals("end") == false) {
                     ingred_list.add(new_ingred);
                 }
+                System.out.println();
 
-                System.out.print("Enter step-by-step cooking instructions (enter \"end\" to quit): \n");
+                System.out.println("Enter step-by-step cooking instructions (enter \"end\" to quit): ");
                 ArrayList<String> instructs = new ArrayList<>();
                 String new_instruc;
                 while ((new_instruc = in.nextLine()).equals("end") == false) {
                     instructs.add(new_instruc);
                 }
+                System.out.println();
 
-                Recipe newRecipe = new Recipe(recipe_name, descript, ingred_list.toArray(new String[1]),
-                        instructs.toArray(new String[1]));
+                Recipe newRecipe = new Recipe(recipe_name, descript, ingred_list, instructs);
+                System.out.println(newRecipe.getName());
             }
         }
         in.close();
--- Recipe.java
+++ Recipe.java
@@ -1,14 +1,15 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Recipe {
     private String name;
     private String descript;
-    private String[] ingred_list;
-    private String[] cook_instruct;
+    private ArrayList<String> ingred_list;
+    private ArrayList<String> cook_instruct;
 
-    public Recipe(String name, String descript, String[] ingred_list, String[] cook_instruct) {
+    public Recipe(String name, String descript, ArrayList<String> ingred_list, ArrayList<String> cook_instruct) {
         this.name = name;
         this.descript = descript;
         this.ingred_list = ingred_list;
@@ -36,20 +37,22 @@ public class Recipe {
             // write description into the file
             try {
                 FileWriter myWriter = new FileWriter(dir, true);
-                myWriter.write(this.descript + "\n");
+                myWriter.write("Description: \n" + this.descript + "\n");
+                myWriter.write(System.getProperty("line.separator"));
                 System.out.println("Successfully wrote description to the file.");
 
                 // write a list of ingredients into the file
 
-                for (int i = 0; i < this.ingred_list.length; i++) {
-                    myWriter.write(this.ingred_list[i] + " ");
-                }
+                myWriter.write("Ingredients: \n" + String.join(", ", this.ingred_list) + "\n");
+                myWriter.write(System.getProperty("line.separator"));
                 System.out.println("Successfully wrote a list of ingredients!");
 
                 // write cooking instructions into the file
-                for (int i = 0; i < this.cook_instruct.length; i++) {
-                    myWriter.write((i + 1) + " " + this.cook_instruct[i] + "\n");
+                myWriter.write("Step-by-step cooking instructions: \n");
+                for (int i = 0; i < this.cook_instruct.size(); i++) {
+                    myWriter.write((i + 1) + ". " + this.cook_instruct.get(i) + "\n");
                 }
+                myWriter.write(System.getProperty("line.separator"));
                 System.out.println("Successfully wrote cooking instructions!");
                 myWriter.close();
 
@@ -60,5 +63,10 @@ public class Recipe {
             System.out.println("Successfully created and saved a new recipe!");
         }
 
+    }
+
+    // get recipe name
+    public String getName() {
+        return this.name;
     }
 }
