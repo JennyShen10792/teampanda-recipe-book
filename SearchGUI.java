337e5a76dbfb2c0001d74e6ae11d29e2a42d5c9a
JennyShen10792 <js10792@nyu.edu>
2022-10-08 18:12:40 -0400

    Start Search function

---
+++ SearchGUI.java
@@ -0,0 +1,169 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.util.*;
+
+public class SearchGUI extends JFrame {
+
+	private JPanel contentPane;
+	//private JTextField searchField;
+	//private JButton searchButton;
+	private List<JButton> recipeButtons;
+	private JTextArea recipeArea;
+	
+	private ArrayList<String> search(String word) {
+        ArrayList<String> matches = new ArrayList<String>();
+        try {
+            File sourceFolder = new File("./Recipe/");
+            for (File f : sourceFolder.listFiles()) {
+                if (f.getName().toLowerCase().contains(word.toLowerCase())) {
+                    matches.add(f.getName().substring(0, f.getName().lastIndexOf(".")));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("error with searching");
+            e.printStackTrace();
+        }
+        return matches;
+
+    }
+	
+	private void recipeBtnClick(String fileName) throws IOException {
+		
+		if (recipeArea == null) {
+			recipeArea = new JTextArea();
+			// add recipeArea to contentPane
+			contentPane.add(recipeArea);
+		}
+		
+		
+		
+		File sourceFolder = new File("./Recipe/" + fileName + ".txt");
+		
+		// Read file
+		//
+		String fileContent = "";
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFolder))) {
+		    String line;
+		    StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters   
+		    while((line=br.readLine())!=null)  
+		    {  
+		    sb.append(line);      //appends line to string buffer  
+		    sb.append("\n");     //line feed   
+		    }  
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		recipeArea.setText(fileContent);
+		
+	}
+	
+	private void searchBtnClick() {
+		
+		for (int i = 0; i < recipeButtons.size(); i++) {
+			
+			contentPane.remove(recipeButtons.get(i));
+		}
+
+		
+		String searchWords = searchField.getText();
+		
+		
+		ArrayList<String> searchResults = search(searchWords);
+		
+		
+		if (! searchResults.isEmpty()) {
+			int x = 40;
+			for (int i = 0; i < searchResults.size(); i++) {
+				
+				final String searchResult = searchResults.get(i);
+				JButton recipeBtn = new JButton(searchResult);
+				recipeButtons.add(recipeBtn);
+				
+				recipeBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						try {
+							recipeBtnClick(searchResult);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});
+				recipeBtn.setBounds(117, x+70, 206, 29);
+				contentPane.add(recipeBtn);				
+			}
+			
+		}
+				
+		
+	}
+	
+	
+	
+	public SearchGUI() {
+		
+		recipeButtons = new ArrayList<JButton>();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel welcomemsg = new JLabel("Search for a Recipe");
+		welcomemsg.setForeground(new Color(240, 128, 128));
+		welcomemsg.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 14));
+		welcomemsg.setBounds(23, 17, 258, 16);
+		contentPane.add(welcomemsg);
+		
+		JTextField searchField = new JTextField();
+		searchField.setBounds(52, 78, 299, 148);
+		contentPane.add(searchField);
+		JButton searchButton = new JButton("Search");
+		searchButton.setBounds(117, 108, 206, 29);
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				searchBtnClick();
+			}
+			
+			
+		});
+		
+		contentPane.add(searchButton);
+		add(contentPane);
+		frame.setVisible(true);
+		
+
+//		 File sourceFolder = new File("./Recipe/");
+//		 StringBuilder builder = new StringBuilder();
+//         for (File f : sourceFolder.listFiles()) {
+//        	 String fileName = f.getName().substring(0, f.getName().lastIndexOf("."));
+//        	 if (fileName.toLowerCase().contains(searchWords.toLowerCase())
+//        	 
+//
+//
+//        	 builder.append(f.getName().substring(0, f.getName().lastIndexOf(".")));
+//             builder.append("\n");
+//	}
+ 
+}
+}
